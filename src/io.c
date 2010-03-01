b5b4aecb5faa95d4d16e7bdeb373f7fdf93e129a
Jorge <salinasv@gmail.com>
2010-03-01 10:52:08 -0600

    Initial support for $XDG_CONFIG files This commit fetch user account info from $XDG_CONFIG/Chronus/account This file must have the following format: account password
    TODO:
    *We need to create the file if it doesn't exist.
    *Move to xml format to be able to have more than 1 account.

---
+++ src/io.c
@@ -0,0 +1,74 @@
+#include <basedir.h>
+#include <stdio.h>
+
+#include <glib.h>
+
+#define CONFIG_FILE "account"
+#define DATA_PREFIX "calendar"
+#define PREFIX "Chronus"
+
+struct xdgHandle *handle;
+
+void xdg_init()
+{
+	handle = g_new(xdgHandle, 1);
+	handle = xdgInitHandle(handle);
+}
+
+void xdg_uninit()
+{
+	xdgWipeHandle(handle);
+	g_free(handle);
+}
+
+char* config_get_user()
+{
+	char *basedir;
+	char *path;
+	FILE *fd;
+	char buffer[100];
+	char *user;
+
+	basedir = xdgConfigHome(handle);
+
+	path = g_build_filename(basedir, PREFIX, CONFIG_FILE, NULL);
+
+	printf("path: %s\n", path);
+
+	/* TODO: Create the file if it doesn't exist */
+	fd = fopen(path, "r");
+	fgets(buffer, 100, fd);
+
+	user = g_strdup(buffer);
+
+	fclose(fd);
+
+	return user;
+
+}
+
+char* config_get_password()
+{
+	char *basedir;
+	char *path;
+	FILE *fd;
+	char buffer[100];
+	char *user;
+
+	basedir = xdgConfigHome(handle);
+
+	path = g_build_filename(basedir, PREFIX, CONFIG_FILE, NULL);
+
+	printf("path: %s\n", path);
+
+	/* TODO: Create the file if it doesn't exist */
+	fd = fopen(path, "r");
+	fgets(buffer, 100, fd);
+	fgets(buffer, 100, fd);
+
+	user = g_strdup(buffer);
+
+	fclose(fd);
+
+	return user;
+}
---
+++ src/io.h
@@ -0,0 +1,33 @@
+#ifndef IO_H
+#define IO_H
+
+/**
+ * Init the XDG subsystem.
+ */
+void xdg_init();
+
+/**
+ * Uninit the XDG subsytem.
+ */
+void xdg_uninit();
+
+/*
+ * Brings the username to be used from $XDG_CONFIG_DIR/Chronus/account
+ * This file must have this format:
+ * username
+ * password
+ *
+ * TODO: change this to xml and support more than one account.
+ *
+ * @return The username. User must free this string.
+ */
+char* config_get_user();
+
+/**
+ * Brings the password from $XDG_CONFIG_DIR/Chronus/account
+ *
+ * @return The user password. User must free this string.
+ */
+char* config_get_password();
+
+#endif /* IO_H */
