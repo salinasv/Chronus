f11a496bf0a6085b5e7e34681bb7de2fe125c469
Jorge <salinasv@gmail.com>
2010-03-04 11:04:43 -0600

    Add initial caldav support using libcaldav.

---
+++ src/caldav.c
@@ -0,0 +1,58 @@
+#include <libcaldav-0.4.0/caldav.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "caldav.h"
+
+DavData* dav_new(const char *user, const char *password, const char *url)
+{
+	DavData *data;
+
+	data = g_new(DavData, 1);
+
+	data->user = g_strdup(user);
+	data->password = g_strdup(password);
+	data->url = g_strdup(url);
+
+	return data;
+}
+
+void dav_destroy(DavData *data)
+{
+	g_free(data->user);
+	g_free(data->password);
+	g_free(data->url);
+
+	g_free(data);
+}
+
+/* I got this code directly from libcaldav's caldav-test.c
+ * and patched it to use DavData*/
+static char* dav_get_uri(DavData *data)
+{
+	char *pos;
+	char *protocol = NULL;
+	char *uri = NULL;
+	char *newurl = NULL;
+	char *url;
+
+	url = data->url;
+
+	if (!data->user)
+		return url;
+	if ((pos = strstr(url, "//")) != NULL) {
+		uri = g_strdup(&(*(pos + 2)));
+		protocol = g_strndup(url, pos + 2 - url);
+	}
+	else {
+	    protocol = g_strdup("http://");
+	}
+	if (!pwd)
+		newurl = g_strdup_printf("%s%s@%s", protocol, data->user, uri);
+	else
+		newurl = g_strdup_printf("%s%s:%s@%s", protocol, data->user, data->password, uri);
+	g_free(uri);
+	g_free(protocol);
+	return newurl;
+}
+
---
+++ src/caldav.h
@@ -0,0 +1,28 @@
+#ifndef CALDAV_H
+#define CALDAV_H
+
+typedef _DavData
+{
+	char *user;
+	char *passw;
+	char *url;
+}DavData;
+
+/**
+ * Creates a new DavData object.
+ *
+ * @param user 		The user to be used to authenticate.
+ * @param password 	The password to be used to authenticate.
+ * @param url 		The calendar provided url.
+ */
+DavData* dav_new(const char *user, const char *password, const char *url);
+
+/**
+ * Destroy a DavData object.
+ *
+ * @param data 	The object to be destroyed.
+ */
+void dav_destroy(DavData *data)
+void dav_get_all_google(char *username, char *password);
+
+#endif /* CALDAV_H */
